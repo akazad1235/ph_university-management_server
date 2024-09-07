ebd13d61205c140c62f8624cce6cf0439b39dcd2
akazad914@gmail.com <akazad914@gmail.com>
2024-09-08 03:00:47 +0600

    Stop tracking node_modules, dist, .env, and .vercel directories

--- dist/moduler/user/user.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserController = void 0;
-const createUser = (req, res) => {
-};
-exports.UserController = {
-    createUser,
-};
